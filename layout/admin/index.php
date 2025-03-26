e193521099cc415f05494deeb218153614e78a4b
Hung dap chai <0258hahung@gmail.com>
2025-03-26 18:14:37 +0700

    quan li nv, quan li kh

--- layout/admin/index.php
+++ layout/admin/index.php
@@ -25,7 +25,7 @@
     <div class="container">
       <?php include './includes/header.php'; ?>
       <?php include './includes/sidebar.php'; ?>
-      <?php include './modules/dathang.php'; ?>
+      <?php include './modules/account.php'; ?>
       </div>
     </div>
 </body>
