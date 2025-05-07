c5c2705ead947a25798f979f82a33d5166af1713
camdao <luongcamdao@gmail.com>
2025-05-07 14:57:27 +0700

    feat: cart

--- JS/client/cartdetail.js
+++ JS/client/cartdetail.js
@@ -1,26 +1,26 @@
 const CART_DETAIL_API_URL = '../../src/router/cartdetailrouter.php';
 
-const addCartDetail = async (productID, quantity, cartID) => {
-    const formData = new URLSearchParams();
-    formData.append('action', 'addCartDetail');
-    formData.append('productID', productID);
-    formData.append('quantity', quantity);
-    formData.append('cartID', cartID);
+// const addCartDetail = async (productID, quantity, cartID) => {
+//     const formData = new URLSearchParams();
+//     formData.append('action', 'addCartDetail');
+//     formData.append('productID', productID);
+//     formData.append('quantity', quantity);
+//     formData.append('cartID', cartID);
 
-    const response = await fetch(CART_DETAIL_API_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: formData.toString(),
-    });
+//     const response = await fetch(CART_DETAIL_API_URL, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//         body: formData.toString(),
+//     });
 
-    if (!response.ok) {
-        throw new Error('Không thể thêm sản phẩm vào giỏ hàng');
-    }
+//     if (!response.ok) {
+//         throw new Error('Không thể thêm sản phẩm vào giỏ hàng');
+//     }
 
-    return await response.json();
-};
+//     return await response.json();
+// };
 
 const getCartDetailsByCartID = async (cartID) => {
     const response = await fetch(`${CART_DETAIL_API_URL}?action=getCartDetailsByCartID&cartID=${cartID}`, {
